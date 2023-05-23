b99f2118ed30665d10bea6c14ba7bf6de4428693
saikiran086 <saikirandulam1997@gmail.com>
2023-05-23 17:13:13 +0530

    hbhcskdjjh

--- src/test/java/tests/StepDef1.java
+++ src/test/java/tests/StepDef1.java
@@ -3,7 +3,10 @@ package tests;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -22,8 +25,9 @@ public class StepDef1 {
 	@Given("open browser")
 	public void openBrowser()
 	{
-		WebDriverManager.chromedriver().setup();
-		sh.driver=new ChromeDriver();
+		
+		WebDriverManager.firefoxdriver().setup();
+		sh.driver=new FirefoxDriver();
 		sh.driver.manage().window().maximize();
 		sh.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 	}
@@ -37,8 +41,8 @@ public class StepDef1 {
 	@Then("search a word {string}")
 	public void searchBox(String word)
 	{
-		sh.driver.findElement(By.cssSelector("input[title='Search']")).sendKeys(word);
-		sh.driver.findElement(By.xpath("(//input[@aria-label='Google Search'])[1]")).click();
+		sh.driver.findElement(By.cssSelector("input[title='Search']")).sendKeys(word,Keys.ENTER);
+		//sh.driver.findElement(By.xpath("(//input[@aria-label='Google Search'])[1]")).click();
 	}
 	
 	@When("page title shoud be {string}")
@@ -49,10 +53,12 @@ public class StepDef1 {
 		if(actualTitle.contentEquals(title))
 		{
 			System.out.println("test pass");
+			Assert.assertTrue(true);
 		}
 		else
 		{
 			System.out.println("terst failed");
+			Assert.assertTrue(false);
 		}
 	}
 	@Then("close site")
@@ -61,6 +67,7 @@ public class StepDef1 {
 		sh.driver.close();
 	}
 }
+
 
 
 
